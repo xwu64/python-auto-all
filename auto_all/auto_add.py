f581ca183a08326256624f375ceb65e4c35c3666
Xiaoliang Wu <zurielibra@gmail.com>
2022-01-19 21:37:46 -0800

    ignore files that have __all__

--- auto_all/auto_add.py
+++ auto_all/auto_add.py
@@ -7,6 +7,23 @@ PREFIX = "__all__ = "
 FRONT_PAD = len(PREFIX) + 1 # one char for "["
 
 
+def is_exist(filename: str) -> bool:
+    with open(filename, 'r') as fh:
+        tree = ast.parse(fh.read())
+        for body in tree.body:
+            if isinstance(body, ast.Assign):
+                for var in body.targets:
+                    print(var.__dict__)
+                    if isinstance(var, ast.Name) and var.id == "__all__":
+                        # assign with one variable
+                        return True
+                    if isinstance(var, ast.Tuple) and any([e.id == "__all__" for e in var.elts]):
+                        # assign with multiple variables
+                        return True
+
+        return False
+
+
 def generate(filename) -> List[str]:
     names = []
 
@@ -57,7 +74,8 @@ def run(filename: str):
     names = generate(filename)
     text = format(names)
     line_num = find_position(filename)
-    insert_to_file_at_line(filename, line_num, text)
+    if not is_exist(filename):
+        insert_to_file_at_line(filename, line_num, text)
 
 
 if __name__ == "__main__":
